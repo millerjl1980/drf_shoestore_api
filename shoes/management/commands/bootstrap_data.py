a5b116797b9594ce53e2f82c6e80c1acf9a4f0b3
Justin Miller <indymiller@gmail.com>
2020-06-18 18:55:15 -0400

    bootstrap_data command complete

--- shoes/management/commands/bootstrap_data.py
+++ shoes/management/commands/bootstrap_data.py
@@ -6,23 +6,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         s_types = ['sneaker', 'boot', 'sandal', 'dress', 'other']
-        type_data = []
+
         for s_type in s_types:
             new_type = ShoeType.objects.create(style=s_type)
             new_type.save()
-            
+        
+        s_colors = [
+            ShoeColor.red,
+            ShoeColor.orange,
+            ShoeColor.yellow, 
+            ShoeColor.green,
+            ShoeColor.blue,
+            ShoeColor.indego,
+            ShoeColor.violet,
+            ShoeColor.black,
+            ShoeColor.white
+        ]
+
+        for s_color in s_colors:
+            new_color = ShoeColor.objects.create(color_name=s_color)
+            new_color.save()
      
         self.stdout.write(self.style.SUCCESS('Shoe types and colors created successfully.'))
-
-           # s_colors = [
-        #     models.red,
-        #     models.orange,
-        #     models.yellow, 
-        #     models.green,
-        #     models.blue,
-        #     models.indego,
-        #     models.violet,
-        #     models.black,
-        #     models.white
-        # ]
-        # ShoeColor.objects.bulk_create(s_colors)
